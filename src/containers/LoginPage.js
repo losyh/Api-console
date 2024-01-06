89ba9a2a41318ae6782a1764c6221f7db374c8fe
Aleksey Petrov <losyh2005@mail.ru>
2024-01-06 19:44:27 +0400

    add layout

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -5,9 +5,7 @@ import styled from 'styled-components';
 
 import {authenticate} from 'src/store/actions/auth';
 
-const Lox = () => {
-  console.log('Sdvsdv')
-}
+console.log(false);
 
 const Wrapper = styled.div`
   height: 100%;
@@ -32,13 +30,89 @@ const LogoStyled = styled.img`
   margin-bottom: 20px;
 `;
 
+const Input = styled.input`
+width: 460px;
+height: 40px;
+border-radius: 5px;
+border: 1px solid var(--borders-elements, rgba(0, 0, 0, 0.20));
+background: #FFF;
+margin: 0 0 20px;
+color: var(--Text-Black, #0D0D0D);
+font-size: 18px;
+font-style: normal;
+font-weight: 400;
+line-height: 30px;
+padding-left: 5px;
+`;
+
+const InputPass = styled(Input)`
+letter-spacing: 1.8px;
+font-weight: 900;
+`;
+
+const Title = styled.h1`
+color: var(--Text-Black, #0D0D0D);
+font-size: 24px;
+font-style: normal;
+font-weight: 400;
+line-height: 30px;
+margin: 0 0 20px;
+`
+
+const SubTitle = styled.p`
+color: var(--Text-Black, #0D0D0D);
+font-size: 16px;
+font-style: normal;
+font-weight: 400;
+line-height: 20px;
+margin: 0 0 5px;
+display: inline-flex;
+`
+
+const Option = styled(SubTitle)`
+color: var(--Text-gray, #999);
+text-align: right;
+font-size: 12px;
+display: inline-flex;
+margin-left: 313px
+`
+const LogBtn = styled.button`
+width: 110px;
+height: 40px;
+border-radius: 5px;
+background: linear-gradient(180deg, #45A6FF 0%, #0055FB 100%), #C4C4C4;
+color: #FFF;
+text-align: center;
+font-size: 16px;
+font-style: normal;
+font-weight: 500;
+line-height: 30px;
+outline: none;
+border: none;
+cursor: pointer;
+`
+
+const LinkToGit = styled.a`
+color: var(--Text-gray, #999);
+text-align: center;
+font-size: 16px;
+font-style: normal;
+font-weight: 400;
+line-height: 20px;
+margin: 20px 0 0; 
+cursor: pointer;
+text-decoration: none;
+`
+
 function LoginPage({history}) {
   const dispatch = useDispatch();
   const [login, setLogin] = useState('');
   const [sublogin, setSubLogin] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
   const loading = useSelector((state) => state.auth.loading);
   const isLoggedIn = useSelector((state) => !!state.auth.sessionKey?.length);
+  
   console.log('loading', loading);
 
   useEffect(() => {
@@ -62,17 +136,25 @@ function LoginPage({history}) {
     doLogin();
   }
 
+  const errorInp = error ? {outline: '1px solid red', boxShadow: '1px 1px 2px 0 red'} : null;
+  const errorP = error ? {color: 'var(--Red, #CF2C00)'} : null;
   return (
     <Wrapper>
-      <LogoStyled src="/icons/logo.svg" alt="" />
+      <LogoStyled src="/icons/logo.svg" alt="logo" />
       <Form onSubmit={onSubmit} action="/">
-        <input value={login} onChange={(e) => setLogin(e.target.value)} placeholder="Логин" />
-        <input value={sublogin} onChange={(e) => setSubLogin(e.target.value)} placeholder="Сублогин" />
-        <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Сублогин" />
-        <button type="submit" onClick={onSubmit}>
-          Отправить
-        </button>
+        <Title>API-консолька</Title>
+        <SubTitle style={errorP}>Логин</SubTitle>
+        <Input value={login} onChange={(e) => setLogin(e.target.value)} style={errorInp}/>
+        <SubTitle style={errorP}>Сублогин</SubTitle>
+        <Option>Опционально</Option>
+        <Input value={sublogin} onChange={(e) => setSubLogin(e.target.value)} style={errorInp}/>
+        <SubTitle style={errorP}>Пароль</SubTitle>
+        <InputPass value={password} onChange={(e) => setPassword(e.target.value)} type='password' style={errorInp}/>
+        <LogBtn type="submit" onClick={onSubmit}>
+          Войти
+        </LogBtn>
       </Form>
+      <LinkToGit href="https://github.com/losyh/main-project" target="_blank">@link-to-your-github</LinkToGit>
     </Wrapper>
   );
 }
