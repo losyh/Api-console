8df20011b4eecd672fb4884a74f7a76d352ea272
Aleksey Petrov <losyh2005@mail.ru>
2024-01-25 21:11:34 +0400

    try format func

---
+++ src/containers/MainPage/ApiConsole/styled.js
@@ -0,0 +1,11 @@
+import styled, {keyframes} from 'styled-components';
+
+export const Input = styled.textarea`
+weight: 500px;
+height: 400px
+`
+
+export const Btn = styled.button`
+weight: 50px;
+height: 40px
+`
--- src/containers/MainPage/MainPage.jsx
+++ src/containers/MainPage/MainPage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {withRouter} from 'react-router-dom';
 import Header from './Header/Header.jsx';
 import HistoryTrack from './HistoryTrack/HistoryTrack.jsx';
+import ApiConsole from './ApiConsole/ApiConsole.jsx';
 
 function MainPage() {
   return (
     <>
       <Header />
       <HistoryTrack />
+      <ApiConsole />
     </>
   );
 }
