35a3abb5e2fefa9aa35dbb1cc222cb0bc20e3ead
Alex <example@gmail.ru>
2024-01-29 16:00:14 +0400

    layout ApiConsole

--- src/containers/MainPage/ApiConsole/ApiConsole.jsx
+++ src/containers/MainPage/ApiConsole/ApiConsole.jsx
@@ -1,17 +1,38 @@
-import React, {useRef} from 'react';
+import React, {useRef, useEffect} from 'react';
 import * as Styled from './styled.js';
 
 const ApiConsole = () => {
-    const inputRef = useRef(null)
+  const inputRef = useRef(null);
+  const dotsRef = useRef(null);
 
-    const handleClickFormatingTextArea = () => {
-        const className = inputRef.current
-        className.value =  JSON.stringify(JSON.parse(className.value), null, 2);
-    }
+  const handleClickFormatingTextArea = () => {
+    const className = inputRef.current;
+    className.value = JSON.stringify(JSON.parse(className.value), null, 2);
+  };
+
+  // useEffect(() => {
+  //   const div = dotsRef.current;
+  //   console.log(dotsRef);
+  //   const listener = (e) => {
+  //     e.preventDefault();
+  //     scroll.offsetLeft += e.pageX - 50 + 'px';
+  //   };
+  //   div.addEventListener('mousedown', (e) => {
+  //     document.addEventListener('mousemove', listener);
+  //   });
+
+  //   return div.addEventListener('mouseup', (e) => {
+  //     document.removeEventListener('mousemove', listener);
+  //   });
+  // }, []);
 
   return (
     <>
-      <Styled.TextArea role='textbox' ref={inputRef}/>
+      <Styled.ApiContainer>
+        <Styled.TextArea role="textbox" ref={inputRef} />
+        <Styled.Dots src="/icons/dots.svg" ref={dotsRef} draggable="true" />
+        <Styled.TextArea role="textbox" />
+      </Styled.ApiContainer>
       <Styled.Btn onClick={handleClickFormatingTextArea}> Нажми </Styled.Btn>
     </>
   );
--- src/containers/MainPage/ApiConsole/styled.js
+++ src/containers/MainPage/ApiConsole/styled.js
@@ -1,11 +1,29 @@
 import styled, {keyframes} from 'styled-components';
 
+export const ApiContainer = styled.section`
+  display: flex;
+  padding: 0 15px;
+  gap: 3px;
+`;
+
 export const TextArea = styled.textarea`
-weight: 500px;
-height: 400px
-`
+  width: 100%;
+  height: 515px;
+  resize: horizontal;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid var(--borders-elements, rgba(0, 0, 0, 0.2));
+  background: #fff;
+  font-family: 'Fira Code';
+  font-size: 14px;
+  line-height: 18px;
+`;
 
 export const Btn = styled.button`
-weight: 50px;
-height: 40px
-`
+  weight: 50px;
+  height: 40px;
+`;
+
+export const Dots = styled.img`
+  cursor: pointer;
+`;
