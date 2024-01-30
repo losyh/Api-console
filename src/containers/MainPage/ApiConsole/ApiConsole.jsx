a45eba7706776583061070a2c1d398d240b96bc9
Aleksey Petrov <losyh2005@mail.ru>
2024-01-30 22:12:03 +0400

    30.01.24

--- src/containers/MainPage/ApiConsole/ApiConsole.jsx
+++ src/containers/MainPage/ApiConsole/ApiConsole.jsx
@@ -1,41 +1,60 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import * as Styled from './styled.js';
+import { Resizable } from 're-resizable';
 
 const ApiConsole = () => {
   const inputRef = useRef(null);
   const dotsRef = useRef(null);
+  const [width, setWidth] = useState('934px')
 
   const handleClickFormatingTextArea = () => {
     const className = inputRef.current;
     className.value = JSON.stringify(JSON.parse(className.value), null, 2);
   };
 
-  // useEffect(() => {
-  //   const div = dotsRef.current;
-  //   console.log(dotsRef);
-  //   const listener = (e) => {
-  //     e.preventDefault();
-  //     scroll.offsetLeft += e.pageX - 50 + 'px';
-  //   };
-  //   div.addEventListener('mousedown', (e) => {
-  //     document.addEventListener('mousemove', listener);
-  //   });
-
-  //   return div.addEventListener('mouseup', (e) => {
-  //     document.removeEventListener('mousemove', listener);
-  //   });
-  // }, []);
 
   return (
     <>
       <Styled.ApiContainer>
-        <Styled.TextArea role="textbox" ref={inputRef} />
-        <Styled.Dots src="/icons/dots.svg" ref={dotsRef} draggable="true" />
-        <Styled.TextArea role="textbox" />
+          <Resizable
+            enable={{ top:false, right:true, bottom:false, left:false, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false }}
+            minWidth={'100px'}
+            maxHeight={'530px'}
+            size={{ width: width}}
+            onResizeStop={(e, direction, ref, d) => {
+              setWidth({
+                width: width + d.width
+              });
+            }}>
+              <Styled.ConTadsa>
+              <TextArea label={'Запрос:'} refff={inputRef} id={1}/>
+            <Styled.Img src="/icons/dots.svg" ref={dotsRef} draggable="true" />
+              </Styled.ConTadsa>
+          </Resizable>
+          
+        <TextArea label={'Ответ:'} id={2}/>
       </Styled.ApiContainer>
-      <Styled.Btn onClick={handleClickFormatingTextArea}> Нажми </Styled.Btn>
+      <Styled.Futter>
+      <Styled.Btn>Отправить</Styled.Btn>
+      <Styled.LinkGit href='https://github.com/losyh' target="_blank">@link-to-my-github</Styled.LinkGit>
+      <Styled.FormatContainer onClick={handleClickFormatingTextArea}>
+            <Styled.Img src="/icons/format.svg"/>
+            <Styled.FormatSpan>Форматировать</Styled.FormatSpan>
+      </Styled.FormatContainer>
+      </Styled.Futter>
     </>
   );
 };
+
+
+const TextArea = (props) => {
+  const {label, refff} = props;
+  return (
+    <Styled.TextAreaContainer>
+        <Styled.Label id={label}>{label}</Styled.Label>
+        <Styled.TextArea role="textbox" ref={refff} id={label}/>
+    </Styled.TextAreaContainer>
+  )
+}
 
 export default ApiConsole;
--- src/containers/MainPage/ApiConsole/styled.js
+++ src/containers/MainPage/ApiConsole/styled.js
@@ -2,14 +2,24 @@ import styled, {keyframes} from 'styled-components';
 
 export const ApiContainer = styled.section`
   display: flex;
-  padding: 0 15px;
+  padding: 0 15px 15px 15px;
+  padding-top: 10px;
   gap: 3px;
+  flex-direction: row;
 `;
+
+export const TextAreaContainer = styled.div`
+display: flex;
+align-items: left;
+justify-content: center;
+flex-direction: column;
+width: 100%;
+`
 
 export const TextArea = styled.textarea`
   width: 100%;
-  height: 515px;
-  resize: horizontal;
+  height: 700px;
+  resize: none;
   padding: 10px;
   border-radius: 5px;
   border: 1px solid var(--borders-elements, rgba(0, 0, 0, 0.2));
@@ -17,13 +27,69 @@ export const TextArea = styled.textarea`
   font-family: 'Fira Code';
   font-size: 14px;
   line-height: 18px;
+
 `;
 
 export const Btn = styled.button`
-  weight: 50px;
+  width: 110px;
   height: 40px;
-`;
-
-export const Dots = styled.img`
+  border-radius: 5px;
+  background: linear-gradient(180deg, #45a6ff 0%, #0055fb 100%), #c4c4c4;
+  color: #FFF;
+  font-family: "SF Pro Text";
+  font-size: 16px;
+  outline: none;
+  border: none;
   cursor: pointer;
 `;
+
+export const Img = styled.img`
+  margin-left: 3px;
+`;
+
+export const Label = styled.label`
+color: var(--Text-gray, #999);
+
+font-size: 12px;
+font-style: normal;
+font-weight: 400;
+line-height: 20px;
+display: block;
+`
+
+export const Futter = styled(TextAreaContainer)`
+border-top: 1px solid var(--borders-elements, rgba(0, 0, 0, 0.2));
+align-items: center;
+flex-direction: row;
+justify-content: space-between;
+padding: 15px 15px 0 15px;
+`
+
+export const LinkGit = styled.a`
+color: var(--Text-gray, #999);
+font-size: 16px;
+font-style: normal;
+font-weight: 400;
+line-height: 20px;
+cursor: pointer;
+text-decoration: none;
+`
+
+export const FormatContainer = styled.div`
+display: inline-flex;
+gap: 8px;
+cursor: pointer;
+`
+
+export const FormatSpan = styled.span`
+color: var(--Text-Black, #0D0D0D);
+font-size: 16px;
+font-style: normal;
+font-weight: 400;
+line-height: 20px;
+`
+
+export const ConTadsa = styled.div`
+display: flex;
+
+`
